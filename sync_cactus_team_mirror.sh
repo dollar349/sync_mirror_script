b04b7cc4ede92963f21c3236674ed30a0e6b8080
“dollar” <dollar.wang@emerson.com>
2016-05-24 11:49:44 +0800

    add --exclude="sstate-cache*"

--- sync_cactus_team_mirror.sh
+++ sync_cactus_team_mirror.sh
@@ -1,4 +1,4 @@
 #!/bin/sh
 sudo fusermount -u /sys_home/for_mount/ &> /dev/null
-sudo mount -t nfs 10.162.247.164:/var/www/html/oe-mirror /sys_home/for_mount/ && sudo rsync -arv /sys_home/for_mount/ /sys_home/space_for_http_server/ && sudo fusermount -u /sys_home/for_mount/
+sudo mount -t nfs 10.162.243.141:/var/www/html/oe-mirror /sys_home/for_mount/ && sudo rsync -arv --exclude="sstate-cache*" /sys_home/for_mount/ /sys_home/space_for_http_server/ && sudo fusermount -u /sys_home/for_mount/
 
